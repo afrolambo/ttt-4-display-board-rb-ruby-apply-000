4308e2402eacece61a4b91ab2d14e06b1d373406
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-06 15:22:40 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,7 +5,7 @@
   def display_board(board)
     print " #{board[0]} | #{board[1]} | #{board[2]} "
     puts "-----------"
-    print " #{board[3]} | #{board[4]} | #{board[5]} "\
+    print " #{board[3]} | #{board[4]} | #{board[5]} "
     puts "-----------"
     print " #{board[6]} | #{board[7]} | #{board[8]} "
   end
