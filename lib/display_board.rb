28ddb83aecf71ba3a370402806e4ae69bf57656f
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-06 15:10:29 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+# I want the rows in the display board to be affected by the board itself.
+
 def display_board(board)
-board = [" "," "," "," "," "," "," "," "," "]
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+board = ['a','b','c','d','e','f','g','h','i']
+  rowA = " #{board[0]} | #{board[1]} | #{board[2]} "
+  rowB = "-----------"
+  rowC = " #{board[3]} | #{board[4]} | #{board[5]} "\
+  rowD = "-----------"
+  rowE = " #{board[6]} | #{board[7]} | #{board[8]} "
 end
